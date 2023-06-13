c514a1aa2cf26ae50ecbe12bf1f003b3c6ed1003
AdrianMunandi <adrian@ex.example.com>
2023-06-13 08:46:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 6;
+const num2 = 10;
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 15;
+const num4 = 11;
+const mod = (num3 % num4);
+
+const numbers = [5, 10, 20];
+const max = Math.max(...numbers);
 // Write your code here
